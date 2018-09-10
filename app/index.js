b6b796d274bd07a0c1368c881382e4d9ed34a6d1
Amanda Bullington <amanda@albull.us>
2018-09-10 15:32:54 -0700

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import Home from './screens/Home';
+
+EStyleSheet.build({
+  $primaryBlue: '#4f6d7a',
+  $white: 'white',
+})
+
+export default () => <Home />;
